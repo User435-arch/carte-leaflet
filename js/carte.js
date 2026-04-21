3cf84e61cc4073a1f88a6ea1e248ff55ee9b6c84
Drissi Anis <anis.drissi@etu.univ-lehavre.fr>
2026-04-21 14:17:44 +0200

    affichage de la normandie

--- js/carte.js
+++ js/carte.js
@@ -1,8 +1,26 @@
 // Initialisation de la carte
-        const map = L.map('map').setView([49.2, 0.5], 8); // Centre approximatif de la Normandie
+var map = L.map('map').setView([49.2, 0.5], 8); // Centre approximatif de la Normandie
 
-        // Fond de carte OpenStreetMap
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 18,
-            attribution: '© OpenStreetMap'
-        }).addTo(map);
+// Design de la carte
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+fetch("json/normandie.json")
+  .then(r => r.json())
+  .then(geojson => {
+    L.geoJSON(geojson).addTo(map);
+});
+
+
+document.getElementById('indicateur').addEventListener('change', function() {
+    const fichier = this.value;
+    chargerJson(fichier);
+    setBoxInfo()
+});
+
+function chargerJson(fichier)
+{
+  console.log(fichier);
+}
