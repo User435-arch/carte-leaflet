f75a40b083bed8864926311bc0c406d65c006d55
Drissi Anis <anis.drissi@etu.univ-lehavre.fr>
2026-04-21 16:13:57 +0200

    affichage des données fonctionnel

--- js/carte.js
+++ js/carte.js
@@ -7,20 +7,79 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
+var geojsonLayer = null;
 fetch("json/normandie.json")
   .then(r => r.json())
   .then(geojson => {
-    L.geoJSON(geojson).addTo(map);
+    geojsonLayer = L.geoJSON(geojson, {
+      onEachFeature: onEachFeature
+    }).addTo(map);
 });
 
+//Fenêtre d'affichage des données
+var info = L.control();
+
+  info.onAdd = function (map) {
+      this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+      this.update();
+      return this._div;
+  };
+
+  // methode pour mettre à jour la fenêtre en utilisant les propriétes passées en paramètre
+  info.update = function (props) {
+    this._div.innerHTML = props
+        ? `<strong>${props.nom}</strong><br>${props.valeur ?? "Aucune donnée"}`
+        : "Survolez une commune";
+  };
+
+  info.addTo(map);
+
+let dataIndicateurCourant = {};
 
 document.getElementById('indicateur').addEventListener('change', function() {
     const fichier = this.value;
     chargerJson(fichier);
-    setBoxInfo()
+    
 });
+
+function onEachFeature(feature, layer) {
+
+    layer.on({
+        mouseover: function (e) {
+            const props = e.target.feature.properties;
+
+            const nom = props.nom || props.NOM || "Commune";
+            const code = props.code || props.INSEE || props.CODE;
+
+            // Récupérer la valeur de l’indicateur courant
+            const valeur = dataIndicateurCourant[code];
+
+            // Mise à jour du L.control()
+            info.update({ nom, valeur });
+
+            // Optionnel : highlight
+            e.target.setStyle({
+                weight: 2,
+                color: "#000",
+                fillOpacity: 0.8
+            });
+        },
+
+        mouseout: function (e) {
+            geojsonLayer.resetStyle(e.target);
+            info.update(); // revient au placeholder
+        }
+    });
+}
+
 
 function chargerJson(fichier)
 {
-  console.log(fichier);
+
+  fetch("json/" + fichier)
+      .then(r => r.json())
+      .then(data => {
+          dataIndicateurCourant = data;
+      });
+
 }
